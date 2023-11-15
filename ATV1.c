bb58e66ccc4b6fa9a1d6b9416c8c1cf4ab674152
Purplerain89z <almirlessa@gmail.com>
2023-11-14 22:15:15 -0300

    primeir comit

---
+++ ATV1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+	
+	setlocale(LC_ALL, "");
+	
+	int quantidadeIngressos = 0;
+	
+	printf("Digite a quatidade de inglessos\n");
+	scanf("%d", &quantidadeIngressos);
+	
+	switch(quantidadeIngressos){
+        case 
+    }
+}
