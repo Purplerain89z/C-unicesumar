b7fac0f1edbfb9fc0c3fa27915178d0241f03788
Purplerain89z <almirlessa@gmail.com>
2023-11-15 12:59:54 -0300

    mais um

---
+++ exercicio7.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main() {
+	
+	setlocale(LC_ALL,"");
+	
+	float dep, juros, ren, total;
+	
+	printf("Quanto deseja depositar.\n");
+	scanf("%f", &dep);
+	printf("Informe a taxa de juros.\n");
+	scanf("%f", &juros);
+	ren = dep * (juros /100);
+	total = dep + ren;
+	printf("Total de dinheiro na sua conta com os rendimentos é %.2f.\n", total);
+	
+	
+	return (0);
+}
