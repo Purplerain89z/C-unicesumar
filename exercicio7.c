f1e6c29d9f620f9cf487bd0341ccae167303cccd
Purplerain89z <almirlessa@gmail.com>
2023-11-15 13:09:00 -0300

    faltou informar os rendimentos separados

--- exercicio7.c
+++ exercicio7.c
@@ -8,12 +8,13 @@ int main() {
 	
 	float dep, juros, ren, total;
 	
-	printf("Quanto deseja depositar.\n");
+	printf("Quanto deseja depositar?\n");
 	scanf("%f", &dep);
 	printf("Informe a taxa de juros.\n");
 	scanf("%f", &juros);
 	ren = dep * (juros /100);
 	total = dep + ren;
+    printf("o total de rendimentos é %.2f.", ren);
 	printf("Total de dinheiro na sua conta com os rendimentos é %.2f.\n", total);
 	
 	
