bb1c8133eed11f231c918ab849ab6dd828cea1c1
Purplerain89z <almirlessa@gmail.com>
2023-11-19 19:59:16 -0300

    cap 2

--- exercicio201.c
+++ exercicio201.c
@@ -1,2 +1,27 @@
 #include <stdio.h>
 #include <math.h>
+#include <locale.h>
+
+int main (){
+	setlocale(LC_ALL, "");
+
+    float raiz;
+    int quadrado, num;
+
+    printf("\nDigite um numero: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0){
+        raiz = sqrt (num);
+        printf("O Numero %d é par.\n", num);
+        printf("A raiz quadrada é %.3f", raiz);
+    } 
+    else{
+        quadrado = num * num;
+        printf ("O numero %d é impar.\n", num);
+        printf ("O quadrado é %d", quadrado);
+    }
+
+    return(0);
+
+}
