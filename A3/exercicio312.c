5a8266e27fadaa19d58744fafeccb023d141c3bd
Purplerain89z <almirlessa@gmail.com>
2023-12-01 18:13:24 -0300

    fatorial

--- A3/exercicio312.c
+++ A3/exercicio312.c
@@ -5,8 +5,9 @@ int main(){
     printf("Informe o número:\n");
     scanf("%d",&num);
     fat = 1;
-    for (i=1; i <= num; i++)
+    for (i=1; i <= num; i++){
     fat = fat * i;
+    }
     printf("O fatorial é: %d\n",fat);
     return(0);
 }
