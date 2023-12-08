e6c8e9c1e6e799db16f65c099a6bdec0de601f44
Purplerain89z <almirlessa@gmail.com>
2023-12-07 23:53:43 -0300

    Aprimorando a repetição e a frase a repetir

--- A3/exercicio325.c
+++ A3/exercicio325.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
 int main(){
-    int i;
+    int i, reptir;
+    char frase [60];
 
-    for(i=0; i<= 30;i++){
-        printf("Aguarde..\n");
+    printf("Informe a frase a ser repitida:\n");
+    scanf("%s",&frase);
+    printf("Informe quantas vezes ela deve ser repitida:\n");
+    scanf("%d",&reptir);
+
+    for(i=0; i<= reptir;i++){
+        printf("%s\n", frase);
     }
 
     return(0);
