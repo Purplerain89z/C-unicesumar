f2bbdfb0f5176d9dec8805e96bdd9440aafebd5e
Purplerain89z <almirlessa@gmail.com>
2023-12-12 06:51:19 -0300

    exemplod e preenchimento de vetor

--- A4/exercicio409.c
+++ A4/exercicio409.c
@@ -1,2 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int main(){
+    int vetorA[10];
+    int i;
+    for (i=0;i<10;i++){
+        printf("Digite o %d elemento do vetor:\n", i);
+        scanf("%d",&vetorA[i]);
+
+    }
+    printf("Vetor preenchidoz\n");
+    for (i=0;i<10;i++){
+        printf("o elemento da posição %d é: %d\n",i,vetorA[i]);
+    }
+    return(0);
+}
